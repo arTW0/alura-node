f5de702b42537811c80dd783cce7457b17935746
arTW0 <arthur.moreira@itec.ufpa.br>
2023-03-14 12:06:21 -0300

    method post ready

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -15,6 +15,37 @@ class LivroController {
       message: "Não foi possível obter a lista de livros",
     })
   }
+
+  static cadastrarLivro = (req, res) => {
+    const { titulo, autor, numeroPaginas, editora } = req.body
+
+    if (!titulo || !autor || !numeroPaginas || !editora) {
+      res.status(400).json({
+        message: "Dados inválidos",
+      })
+      return
+    }
+
+    const livro = new livros({
+      titulo,
+      autor,
+      numeroPaginas,
+      editora
+    })
+
+    livro
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          data: result,
+        })
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Não foi possível cadastrar o livro",
+        })
+      })
+  }
 }
 
 export default LivroController
